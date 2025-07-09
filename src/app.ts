cde4a0e93fa120f7f3383fce414460852e71c253
Talha Khan <talha4khan@hotmail.com>
2025-07-09 22:29:17 +0500

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  //const err = createHttpError(401, 'You cannot access this route');
   res.send('welcome to auth service');
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.info(err.message);
+  // You can customize statusCode extraction if you have custom error types
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
-    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    app.listen(PORT, () => {
+      logger.info(`server starting on port ${PORT}`);
+    });
   } catch (err) {
     console.error(err);
     process.exit(1);
